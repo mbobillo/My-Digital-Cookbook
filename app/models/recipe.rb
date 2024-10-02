52d5917863dfad3bdd426d345d1aec0b056b1601
Marion Bobillo <marion.bobillo@gmail.com>
2024-10-02 18:02:02 +0200

    tags

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,6 @@
 class Recipe < ApplicationRecord
+  has_many :recipe_ingredients
+  has_many :recipe_tags
   has_many :ingredients, through: :recipe_ingredients
   has_many :tags, through: :recipe_tags
   belongs_to :category
