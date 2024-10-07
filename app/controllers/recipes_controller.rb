a5e3fb92f97ed8ae5a5f5acae3043dbd3b1d698c
Marion Bobillo <marion.bobillo@gmail.com>
2024-10-07 18:23:35 +0200

    new update

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -5,6 +5,29 @@ class RecipesController < ApplicationController
     @recipe = Recipe.new
   end
 
+  def create
+    @recipe = Recipe.new(recipe_params)
+
+    if params[:recipe_ingredients].present?
+      @ingredients_list = params[:recipe_ingredients].split(',').map(&:strip).uniq
+    else
+      @ingredients_list = []
+    end
+
+    Rails.logger.debug "Ingredients list: #{@ingredients_list.inspect}"
+
+    if @recipe.save
+      @ingredients_list.each do |ingredient_name|
+        ingredient = Ingredient.find_or_create_by(name: ingredient_name)
+        RecipeIngredient.create(recipe: @recipe, ingredient: ingredient)
+      end
+
+      redirect_to recipes_path, notice: 'Recette créée avec succès'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   def index
     @recipes = Recipe.all
 
@@ -42,37 +65,23 @@ class RecipesController < ApplicationController
     @selected_tags = @recipe.tags
   end
 
-  def create
-    @recipe = Recipe.new(recipe_params)
-
-    @ingredients_list = params[:recipe][:ingredients].split(',').map(&:strip).uniq
-
-    if @recipe.save
-      @ingredients_list.each do |ingredient_name|
-        ingredient = Ingredient.find_or_create_by(name: ingredient_name)
-
-        RecipeIngredient.create(recipe: @recipe, ingredient: ingredient)
-      end
-
-      redirect_to @recipe, notice: 'Recipe was successfully created.'
-    else
-      render :new, status: :unprocessable_entity
-    end
+  def edit
   end
 
-  def edit
+  def update
     @recipe.upate(params[:recipe])
+    redirect_to recipes_path(@recipe), notice: 'Recette modifiée avec succès'
   end
 
   def destroy
     @recipe.destroy
-    redirect_to recipes_path, status: :see_other
+    redirect_to recipes_path, status: :see_other, notice: "Recette supprimée avec succès"
   end
 
   private
 
   def recipe_params
-    params.require(:recipe).permit(:title, :main_ingredient, :category_id, images: [])
+    params.require(:recipe).permit(:title, :main_ingredient, :category_id, :recipe_ingredients, :source_url, images: [], tags: [])
   end
 
   def set_recipe
